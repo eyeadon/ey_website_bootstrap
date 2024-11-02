1f5a81a23c029efc3c2e7d9149c0f5250a5c73bf
eyeadon <eyeadon40@gmail.com>
2024-11-02 14:26:06 -0400

    redid contact page; footer edit; added pdf of resume

--- contact.php
+++ contact.php
@@ -1,50 +1,75 @@
 <?php
-  $upOutOfFolder = '';
-  require_once('head1_meta.php');
+$upOutOfFolder = '';
+require_once('head1_meta.php');
 ?>
 
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="Evan Yeadon">
-    <title>Evan Yeadon | Contact</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="Evan Yeadon">
+<title>Evan Yeadon | Contact</title>
 
 <?php
-  require_once('head2_link.php');
-  require_once('header_and_nav.php');
+require_once('head2_link.php');
+require_once('header_and_nav.php');
 ?>
 
 <body>
 
-<!-- Main Content -->
-<div class="container">
-      <h1>Contact</h1>
-      <div class="row rowPadding">
-        <div class="col-md-offset-2 col-md-5">
-		  <p>For questions, quotes, or samples please contact:</p>
-		  <strong><p>Evan Yeadon</p></strong>
-		  <p>Tel. 617-838-8646</p>
-		  <p>eyeadon40@gmail.com</p>
-		  <!-- <p><a href="#" target="_blank">My R&eacute;sum&eacute;</a></p> -->
-        </div>    
-        <div class="col-md-3">
-		  <img src="images/evan-portrait1.jpeg" alt="evan smiling by an ocean scene" class="img-responsive" />
-        </div>
+  <!-- Main Content -->
+  <div class="container">
+    <h1>Contact</h1>
+    <div class="row rowPadding">
+      <div class="col-md-offset-1 col-md-4">
+        <p><strong>Evan Yeadon</strong></p>
+        <p>Phone: 617-838-8646</p>
+        <p>Email: info@evanyeadon.com</p>
+        <p><a href="docs/Evan_Yeadon_Resume.pdf" target="_blank">R&eacute;sum&eacute;</a></p>
       </div>
-      <div class="row bottomPadding">
-        <div class="col-md-offset-2 col-md-8">
-		<h3>Bio</h3>
-		<p>Evan Yeadon is a visual communicator who is constantly influenced by the environments that surround him. A native of rural Massachusetts, landscapes controlled by nature are a great subject for the artist's study of form. The influence of the modern American city is also seen in his compositions that reflect order, geometry, and pattern. His ideas start with a sketch, a collage of photos or a palette of colors. Next the combination of traditional drawing and digital graphics link together to create a design.</p> 
-		<p>Evan holds a B.F.A. in Industrial Design and a Certificate in Web Development. He has a broad range of experience working in a variety of design offices. Specializing in graphic design and web development, he finds innovative solutions that build a strong brand image. Evan has developed identities for human resources/benefits departments, insurance brokerages, and health management programs. His latest work includes the development of WordPress websites.</p> 
-        </div>
+      <div class="col-md-offset-1 col-md-4">
+        <img src="images/evan-portrait1.jpeg" alt="evan smiling by an ocean scene" class="img-responsive" />
+      </div>
+
+    </div>
+    <div class="row bottomPadding">
+      <div class="col-md-offset-1 col-md-4">
+        <p><strong>PROFILE: </strong><br />Knowledgeable designer/web developer seeking skill advancement in web
+          computer programming through collaborative projects.</p>
+      </div>
+      <div class="col-md-offset-1 col-md-4">
+        <p><strong>EDUCATION: </strong><br />
+          B.F.A. in Industrial Design, MassArt<br />
+          Certificate in Web Development, UMass Lowell</p>
       </div>
     </div>
-<!-- End Main Content -->
 
-<?php
+    <div class="row bottomPadding">
+      <div class="col-md-offset-1 col-md-4">
+        <p><strong>SKILL SETS:</strong><br />
+          <strong>Website Design and Development: </strong>JavaScript web apps, Drupal, WordPress, WooCommerce, visual
+          design, site plans, responsive websites
+        </p>
+        <p><strong>Graphic Design: </strong>brand identity, advertising, brochures, marketing collateral, social media
+          graphics</p>
+      </div>
+      <div class="col-md-offset-1 col-md-4">
+        <p><strong>TOOLS:</strong><br />
+          <strong>Programming Languages/Technologies: </strong>HTML, CSS, JavaScript, React, Node.js, jQuery, PHP, Git
+        </p>
+        <p><strong>Software: </strong>Adobe Creative Suite (Acrobat, Illustrator, InDesign, Photoshop, Premiere), Google
+          Workspace, Marketing Cloud, Microsoft Office (Excel, PowerPoint, Word, Teams), Visual Studio Code, Zoom. Mac
+          and
+          Windows.</p>
+      </div>
+    </div>
+
+    <!-- End Main Content -->
+  </div>
+
+  <?php
   require_once('footer.php');
   require_once('bootstrap_core_javascript.php');
-?>
+  ?>
 
-  </body>
+</body>
+
 </html>
-
