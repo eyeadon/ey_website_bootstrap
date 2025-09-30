6adf06ec06bd044f4fb72b125fe4b0864f4d7bfc
eyeadon <eyeadon40@gmail.com>
2025-09-30 12:18:26 -0400

    weather app page

--- web_apps/weather_app.php
+++ web_apps/weather_app.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="author" content="Evan Yeadon">
-    <title>Evan Yeadon | Game Hub</title>
+    <title>Evan Yeadon | Weather and Air Quality App</title>
 
 <?php
   require_once('../head2_link.php');
@@ -19,12 +19,12 @@
     <!-- Main Content -->
     <div class="container">
           <div class="pull-left">
-            <h1>Game Hub</h1>
+            <h1>Weather and Air Quality App</h1>
           </div>
           <div class="pull-right">
             <ol class="breadcrumb">
               <li><a href="../web_apps.php">Web Apps</a></li>
-              <li class="active">Game Hub</li>
+              <li class="active">Weather and Air Quality App</li>
             </ol>
           </div>
         </div> 
@@ -32,24 +32,24 @@
         <div class="container">
           <div class="row bottomPadding">
             <div class="col-md-8 col-md-offset-2">
-            <a href="https://game-hub-evan-yeadons-projects.vercel.app" target="_blank"><img class="img-responsive center-block imageBorder" src="game-hub-screen-900.png" alt="game search app"></a>
+            <a href="https://weather-widget-lemon.vercel.app" target="_blank"><img class="img-responsive center-block imageBorder" src="weather-app-screen.png" alt="local weather and air quality readings"></a>
             </div>
           </div>
           <div class="row bottomPadding">
             <div class="col-md-8 col-md-offset-2">
-            <h2>React Web App</h2>
-              <h3>Technologies: React, Chakra UI, Responsive Design</h3>
+            <h2>JavaScript Web App</h2>
+              <h3>Technologies: Node.js, Express, Tailwind CSS, Responsive Design</h3>
               <h3>Role: Web Designer/Developer</h3>
-              <p><a class="btn btn-primary" href="https://game-hub-evan-yeadons-projects.vercel.app" role="button" target="_blank">View App &raquo;</a></p>
-              <p>This app allows users to search for and find information about video games from the RAWG database. It fetches data from the RAWG API and allows users to search and sort games using a variety of filters. The app features Chakra UI and React for the front end. Additional game description pages were added using React Router. Axios handles fetch requests and React Query and Zustand play roles in state management.</p>
+              <p><a class="btn btn-primary" href="https://weather-widget-lemon.vercel.app" role="button" target="_blank">View App &raquo;</a></p>
+              <p>A simple app displaying local weather and air quality readings. Uses the browser's geolocation data to pass latitude and longitude through an Express API.</p>
             </div>
           </div>
         </div> <!-- /container -->
         
         <nav aria-label="" class="bottomPadding">
           <ul class="pager">
-            <li><a href="whatcha_bringing.php"><span aria-hidden="true">&larr;</span> Previous Project</a></li>
-            <li><a href="issue_tracker.php">Next Project <span aria-hidden="true">&rarr;</span></a></li>
+            <li><a href="issue_tracker.php"><span aria-hidden="true">&larr;</span> Previous Project</a></li>
+            <!-- <li><a href="#">Next Project <span aria-hidden="true">&rarr;</span></a></li> -->
           </ul>
         </nav>
     <!-- End Main Content -->
